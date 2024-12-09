12b0c6f3894baf2115263ac6a653d76253776b31
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2024-12-09 11:19:01 +0530

    drawn a rectangle

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var context = canvas.getContext('2d');
+context.fillRect(100,100,100,100);
